1afd35ab24beda8555eb730b70a65ec9bab0ac02
Rogério Sandim <sandimroger@gmail.com>
2020-09-10 15:05:22 +0100

    Completing challenges

--- typeorm-upload/src/repositories/TransactionsRepository.ts
+++ typeorm-upload/src/repositories/TransactionsRepository.ts
@@ -11,7 +11,26 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const { income, outcome } = transactions.reduce(
+      (acc: Balance, transaction: Transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += Number(transaction.value);
+        }
+        if (transaction.type === 'outcome') {
+          acc.outcome += Number(transaction.value);
+        }
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
